6d2c998deb3b37f511d6678b64778d77a9fb30d6
Seonghyeon Kim <tudgus010707@naver.com>
2023-03-31 01:27:29 +0900

    import logo

--- src/App.js
+++ src/App.js
@@ -1,69 +1,92 @@
-import {useState} from 'react'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function Header(props){
-  return <header>
-    <h1><a href="/">{props.title}</a></h1>
-  </header>
-
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
 }
-function Nav(props){
-  const lis=[]
-  for(let i=0;i<props.topics.length;i++){
-    let t=props.topics[i]
-    lis.push(<li key={t.id}><a id={t.id} href={"/read/"+t.id} onClick={event=>{
-      event.preventDefault();
-      props.onChangeMode(Number(event.target.id));
-    }}>{t.title}</a></li>)
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
   }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
 }
-function Article(props){
-  return <article>
-    <p>
-      <h2>{props.title}</h2>
-    </p>{props.body}
-
-  </article>
+function Article(props) {
+  return (
+    <article>
+      <p>
+        <h2>{props.title}</h2>
+      </p>
+      {props.body}
+    </article>
+  );
 }
-function App(){
-  const [mode, setMode]=useState('WELCOME');
-  const [id, setId]=useState(null);
-  const topics=[
-    {id:1, title:'html', body:'html is...'},
-    {id:2, title:'css', body:'css is...'},
-    {id:3, title:'javascript', body:'javascript is...'}
-  ]
-  let content=null;
-  if(mode==='WELCOME'){
-    content=<Article title='Welcome' body='Hello, WEB'></Article>
-  } else if(mode==='READ'){
-    let title, body=null;
-    for(let i=0;i<topics.length;i++){
-      if(topics[i].id===id){
-        title=topics[i].title;
-        body=topics[i].body;
+function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "javascript", body: "javascript is..." },
+  ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
       }
     }
-    content=<Article title={title} body={body}></Article>
-
+    content = <Article title={title} body={body}></Article>;
   }
-  return(
+  return (
     <div>
-    <Header title='REACT' onChangeMode={()=>{
-      setMode('WELCOME');
-    }}></Header>
-    <Nav topics={topics} onChangeMode={(_id)=>{
-      setMode('READ');
-      setId(_id);
-    }}
-    ></Nav>
-    {content}
-    {/* <Article title={title} body='html is...'></Article> */}
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
+      {/* <Article title={title} body='html is...'></Article> */}
     </div>
-  )
+  );
 }
 export default App;
